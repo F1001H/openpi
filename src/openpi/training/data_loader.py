49cb8c4a35c9c51756669aee7551d0f0e7e2a24c
Fabian Hahne <hahnefabian@gmail.com>
2026-05-27 13:44:34 +0200

    updated for lerobot dataset v3

--- src/openpi/training/data_loader.py
+++ src/openpi/training/data_loader.py
@@ -7,7 +7,7 @@ from typing import Literal, Protocol, SupportsIndex, TypeVar
 
 import jax
 import jax.numpy as jnp
-import lerobot.common.datasets.lerobot_dataset as lerobot_dataset
+import lerobot.datasets.lerobot_dataset as lerobot_dataset
 import numpy as np
 import torch
 
@@ -136,6 +136,8 @@ def create_torch_dataset(
         raise ValueError("Repo ID is not set. Cannot create dataset.")
     if repo_id == "fake":
         return FakeDataset(model_config, num_samples=1024)
+    
+    repo_root = data_config.repo_root
 
     dataset_meta = lerobot_dataset.LeRobotDatasetMetadata(repo_id)
     dataset = lerobot_dataset.LeRobotDataset(
